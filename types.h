0951a551f19b51eed537c48cf1d656628dcb77ef
Imrich Štoffa <xstoff02@stud.fit.vutbr.cz>
2015-11-23 09:17:57 +0100

    Altered token_t for easier parsing

--- types.h
+++ types.h
@@ -17,8 +17,8 @@ typedef enum errv_e
 {
 	COMP_OK = 0,
 	LEX_ERR = 1,
-	SYN_ERR,					//!< Invalid program stucture error
-	SEM_ERR,					//!< Semantic error, undefined or redefinition of function/variable 
+	SYN_ERR,			//!< Invalid program stucture error
+	SEM_ERR,			//!< Semantic error, undefined or redefinition of function/variable
 	SEM_TYPE_ERR,
 	AMB_TYPE_ERR,
 	SEM_OTHER_ERR,
@@ -31,30 +31,13 @@ typedef enum errv_e
 
 /*! \enum tokend_kind_e 
  *	\brief Basic types of tokens
- * 	Scanner shall return tokens that belongs to one of these types
+ *	Scanner shall return tokens that belongs to one of these types
  */
 typedef enum token_kind_e
 {
-	KEYWORD_TK = 0,
-	INT_TK,
-	REAL_TK,
-	LITERAL_TK,
-	OPERATOR_TK,
-	IDENT_TK,
-	CONTROL_TK,
-	BRACE_TK,
-	UNKNOWN_TK
-}token_kind_t;
+	END_TK = 0,
 
-/*! \enum keyword_e
- *	\brief Ordinal values for keywords 
- *  On keyword detection scanner shall return token of kind KEYWORD_TK with 
- *  specific value from these
- *  No need to allocate string for type of keyword that never changes
- */
-typedef enum keyword_e
-{
-	AUTO_KW = 0,
+	AUTO_KW,
 	CIN_KW,
 	COUT_KW,
 	INT_KW,
@@ -63,52 +46,46 @@ typedef enum keyword_e
 	IF_KW,
 	ELSE_KW,
 	FOR_KW,
-	RETURN_KW
-}keywd_t;
+	RETURN_KW,
 
-/*! \enum operator_e 
- *	\brief Ordinal values for operators
- *	On operator detection scanner shall return token of kind OPERATOR_TK with
- *	specific value from these
- */
-typedef enum operator_e
-{
-	ASTERISK_OP = 0,		//!< "*"
-	SLASH_OP,				//!< "/"
-	PLUS_OP, 
+	INT_TK,
+	REAL_TK,
+	LITERAL_TK,
+
+	ASTERISK_OP,		//!< "*"
+	SLASH_OP,			//!< "/"
+	PLUS_OP,
 	MINUS_OP,
-	SEMI_OP, // semicolon
-	COM_OP, // comma
-	DBL_LESS_OP, // <<
-	DBL_GRE_OP, // >>
-	LESS_OP,				//!< "<"
-	GREAT_OP,				//!< ">"
-	LEE_OP,					//!< "<="
-	GRE_OP,					//!< ">="
-	EQ_OP,					//!< "=="
-	NEQ_OP,					//!< "!="
-	ASSIGN_OP				//!< "="
-}operator_t;
+	SEMI_OP,			//! ";" semicolon
+	COM_OP,				//! "," comma
+	DBL_LESS_OP,		//! "<<"
+	DBL_GRE_OP,			//! ">>"
+	LESS_OP,			//!< "<"
+	GREAT_OP,			//!< ">"
+	LEE_OP,				//!< "<="
+	GRE_OP,				//!< ">="
+	EQ_OP,				//!< "=="
+	NEQ_OP,				//!< "!="
+	ASSIGN_OP,			//!< "="
+	
+	BLOCKL_OP,			//!< "{"
+	BLOCKR_OP,			//!< "}"
+	BRACEL_OP,			//!< "("
+	BRACER_OP,			//!< ")"
+	//SQ_BRACEL_OP,			//!< "["
+	//SQ_BRACER_OP,			//!< "]"
 
-/*! \enum brace_e
- *	\brief Ordinal values for braces
- *	On brace detection scanner shall return BRACE_TK 
- */
-typedef enum brace_e
-{
-	BLOCKL_OP,					//!< "{"
-	BLOCKR_OP,					//!< "}"
-	BRACEL_OP,					//!< "("
-	BRACER_OP,					//!< ")"
-	SQ_BRACEL_OP,				//!< "["
-	SQ_BRACER_OP				//!< "]"
-}brace_t;
+	IDENT_TK,
+
+	UNKNOWN_TK
+}token_kind_t;
 
 /*! \struct token_s
  *	\brief Structure to represent token returned by scaner
  *	Token contains a morfing variable. Data member is interpreted according to token
  *	type
  */
+
 typedef struct token_s
 {
 	token_kind_t type;
@@ -117,18 +94,16 @@ typedef struct token_s
 		int ord;
 		double real;
 		char* lit;
-		keywd_t kwd;
-		operator_t oprtr;
-		brace_t brct;
-		xtable_t* symbol;
 	}data;
 }token_t;
 
+typedef struct var_s{
+	token_kind_t type;
+	char* ident;
+}var_t;
+
 //! \brief No chceck memory allocation exit on failure
 #define Galloc(ptr, size) ( (ptr)=malloc(size) != NULL ? 0 : exit(99), 1)
-
-
-
 
 #endif //IFJ_TYPES_H
 
