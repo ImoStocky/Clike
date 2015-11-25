8996d5c9c5a2c7ac96d3cf2a09dd1cd6e8e50c8c
Imrich Štoffa <xstoff02@stud.fit.vutbr.cz>
2015-11-25 01:21:20 +0100

    Draft for syntax analysis, etc.

--- ads.c
+++ ads.c
@@ -7,29 +7,125 @@
 
 #include "ads.h"
 #include <stdbool.h>
-
-//! \define Rounds bytelen to smallest possible multiple of bytesize of boundary type
-#define aligned(bytelen, boundary)\
-	(bytelen/sizeof(boundary)+(length%sizeof(boundary) > 0 ? 1 : 0))
+#include <string.h>
 
 static uint32_t sdbm(unsigned char* str)
 {
 	uint32_t hash = 0;
 	int c;
-
 	while (c = *str++)
 		hash = c + (hash << 6) + (hash << 16) - hash;
-
 	return hash;
 }
 
-int xstack_init(stack_t** st, size_t size)
+// Add stack with default type of int, 
+// Chceck implementation
+
+int xs_init(xstack_t** st, size_t size)
 {
-	if(st != NULL && *st != NULL){
-		*st = malloc(sizeof(stack_t));
-		void* tmp = malloc(aligned(size, uint32_t)*sizeof(uint32_t));
-		if(*st != NULL && tmp != NULL){
-			(*st)->size = aligned(size, uint32_t); //!< number of dwords
+	*st = malloc(sizeof(xs_t));
+	if(*st != NULL){
+		void* tmp = malloc(al4B(size)*sizeof(base_t));
+		if(tmp != NULL){
+			(*st)->size = al4B(size); //!< number of dwords
+			(*st)->top = 0;
+			(*st)->data = tmp;
+			return st;
+		}
+		free(*st);
+		free(tmp);
+	}
+	return 1;
+}
+
+void xs_del(xstack_t** st)
+{
+	free((*st)->data);
+	free((*st);
+	*st = NULL;
+}
+			
+
+int xs_resize(xstack_t* st, int size)
+{
+	unsigned ne = al4B(size);
+	if(st->top < ne){
+		void* tmp = realloc(st->data, ne*sizeof(st->data[0]));
+		if(tmp != NULL){
+			st->data = tmp;
+			st->size = ne;
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int xs_push(xstack_t* st, void* data, int length)
+{
+	unsigned ne = al4B(length);
+	if(st->top >= ne && st->top + ne <= st->size){
+		memcpy(st->data+st->top, data, length);
+		st->top += ne; 
+		return 0;
+	} else {
+		while(ne + st->top > st->size){
+			int error = xs_resize(st, st->size*2);
+			if(error){
+				fprintf(stderr, "Failed to allocate %u memory for xs_t\n",st->size*2);
+				return 1;
+			}
+		}
+		memcpy(st->data+st->top, data, length);
+		st->top += ne; 
+		return 0;
+	}
+	return 1;
+}
+
+int xs_pop(xstack_t* st, int length)
+{
+	unsigned ne = al4B(length);
+	if(st->top >= ne && st->top + ne <= st->size){
+		st->top -= ne;
+		return 0;
+	}
+	return 1;
+}
+
+int xs_top(xstack_t* st, void* data, int length)
+{	
+	unsigned ne = al4B(length);
+	if(st->top >= ne && st->top + ne <= st->size){
+		memcpy(data, st->data+st->top-ne, length);
+		return 0;
+	}
+	return 1;
+}
+
+int xs_tpop(xstack_t* st, void* data, int length){
+
+	unsigned ne = al4B(length);
+	if(st->top >= ne && st->top + ne <= st->size){
+		memcpy(data, st->data+st->top-ne, length);
+		st->top -= ne;
+		return 0;
+	}
+	return 1;
+}
+
+int xs_empty(xstack_t* st)
+{	
+	return(st->top == 0);
+}
+
+
+int si_init(istack_t** st, size_t size)
+{
+	*st = malloc(sizeof(istack_t));
+	if(*st != NULL){
+		void* tmp = malloc(size*sizeof(int));
+		if(tmp != NULL){
+			(*st)->size = size; //!< number of dwords
 			(*st)->top = 0;
 			(*st)->data = tmp;
 			return 0;
@@ -40,81 +136,62 @@ int xstack_init(stack_t** st, size_t size)
 	return 1;
 }
 
-int xstack_resize(stack_t* st, int size)
+void si_del(istack_t** st)
 {
-	int ne = aligned(size, uint32_t);
-	if(st->top <= ne){
-		void* tmp = realloc(st->data, ne*sizeof(uint32_t));
+	free((*st)->data);
+	free((*st);
+	*st = NULL;
+}
+			
+
+int si_resize(istack_t* st, int size)
+{
+	if(st->top < size){
+		void* tmp = realloc(st->data, size*sizeof(st->data[0]));
 		if(tmp != NULL){
 			st->data = tmp;
-			st->size = ne;
+			st->size = size;
 			return 0;
 		}
 	}
 	return 1;
 }
 
-int xstack_push(xstack_t* st, void* data, int length)
+void si_push(istack_t* st, int data )
 {
-	int ne = aligned(length, uint32_t);
-	if(st->top >= ne && st->top + ne <= st->size){
-		memcpy(st->data+st->top, data, length);
-		st->top += ne; 
-		return 0;
+	if(st->top + 1 < st->size){
+		*(st->data + st->top) = data;
+		st->top++; 
 	}
-	return 1;
 }
 
-int xstack_pop(xstack_t* st, int length)
+void si_pop(istack_t* st)
 {
-	int ne = aligned(length, uint32_t);
-	if(st->top >= ne && st->top + ne <= st->size){
-		st->top -= ne;
-		return 0;
-	}
-	return 1;
+	if(st->top > 0) 
+		st->top-- ;
 }
 
-int xstack_top(xstack_t* st, void* data, int length)
+void si_top(istack_t* st, int* data)
 {	
-	if(st->top >= ne && st->top + ne <= st->size){
-		memcpy(data, st->data+st->top-ne, length);
-		return 0;
+	if(st->top > 0){
+		*data = *(st->data + st->top-1);
 	}
-	return 1;
 }
 
-bool xstack_empty(xstack_t* st)
+void si_tpop(istack_t* st, int* data){
+
+	if(st->top > 0){
+		st->top-- ;
+		*data = *(st->data + st->top);
+	}
+}
+
+int si_empty(istack_t* st)
 {	
 	return(st->top == 0);
 }
 
-xtable_t *xtable_init(size_t size)
+int si_full(istack* st)
 {
-	
-}
-
-int xtable_insert(xtable_t* tab, int key, void* data)
-{
-	
-}
-
-bool xtable_search(xtable_t* tab, int key)
-{
-	
-}
-
-int xtable_delete(xtable_t* tab, int key)
-{
-
-}
-
-int xtable_copy(xtable_t* tab, int key, void* data)
-{
-
-}
-
-int xtable_resize(xtable_t* tab, size_t num)
-{
-
+	return(st->top < st->size);
 }
--- ads.h
+++ ads.h
@@ -5,4 +5,65 @@
  *	for compiler machinery to work flawlessly
  */
 
+#include <stdint.h>
 
+#define al4B(bytelen)\
+	(bytelen/sizeof(base_t)+(bytelen%sizeof(base_t) > 0 ? 1 : 0))
+
+
+#define ELNb(d, i) (i/8*sizeof(d[0]))
+#define bIDX(d, i) (i%8*sizeof(d[0]))
+#define GETb(d, i) (d[ELNb(d,i)] & 1LL << bIDX(d,i))
+#define SETb(d, i, m) (d[ELNb(d,i)] = ((d[ELNb(d,i)] & ~(m << bIDX(d,i))) | (-d[ELNb(d,i)] & (m << bIDX(d,i)))) )
+
+typedef enum symbol_type_e
+{
+	FUNC_TP = 1,
+	VAR_TP	
+}symbol_type_t;
+
+typedef struct symbol_s{
+		char* ident;
+		int s_type;		
+		int d_type;
+		union{
+			void* var_addr; 
+			stack_t func_params;
+		}
+}symbol_t;
+
+typedef uint32_t base_t;
+
+typedef struct xstack_s{
+	unsigned size;
+	unsigned top;
+	base_t *data;
+}xstack_t;
+
+typedef struct istack_s{
+	unsigned size;
+	unsigned top;
+	int *data;
+}istack_t;
+
+
+
+int sx_init(xstack_t** st, size_t size);
+void sx_del(xstack_t** st);
+int sx_resize(xstack_t* st, int size);
+int sx_push(xstack_t* st, void* data, int length);
+int sx_pop(xstack_t* st, int length);
+int sx_top(xstack_t* st, void* data, int length);
+int sx_tpop(xstack_t* st, void* data, int length);
+int sx_empty(xstack_t* st);
+
+
+int si_init(istack_t** st, size_t size);
+void si_del(istack_t** st);
+int si_resize(istack_t* st, int size);
+void si_push(istack_t* st, int data );
+void si_pop(istack_t* st);
+void si_top(istack_t* st, int* data);
+void si_tpop(istack_t* st, int* data);
+int si_empty(istack_t* st);
+int si_full(istack* st);
